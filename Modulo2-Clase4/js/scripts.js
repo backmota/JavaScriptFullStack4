e498a82d29f72b1a4fb076714b854eee6c357236
José Juan Mota <backmota@BackChai.local>
2022-11-09 20:09:54 -0600

    Clase 4

---
+++ Modulo2-Clase4/js/scripts.js
@@ -0,0 +1,16 @@
+//const clients = [];
+//https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+const clients = [];
+
+clients[0] = ["Mango", "Poly", ["1", "2", "3"]];
+clients[5] = 5;
+clients[4] = "otro usuario";
+clients[3] = "otro usuario";
+
+
+console.log(clients[0][2][2]);
+
+console.log(clients.length-1);
+
+
