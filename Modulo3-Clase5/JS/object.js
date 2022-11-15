c427c78a34998bc0d64b9396ad73eb9538e427b9
José Juan Mota <backmota@BackChai.local>
2022-11-14 21:14:51 -0600

    objetos

--- Modulo3-Clase5/JS/object.js
+++ Modulo3-Clase5/JS/object.js
@@ -1,3 +1,12 @@
+//Los objetos permiten describir y agrupar las características de una entidad: un usuario, un libro, un producto en una tienda, cualquier cosa. Los objetos también se llaman diccionarios, es decir, contienen términos (propiedades) y sus definiciones (valores).
+/*
+Cuando se crea un objeto, se pueden añadir propiedades, cada una descrita por pares clave:valor. La clave también se llama nombre de la propiedad y siempre es una cadena. El valor de una propiedad puede ser de cualquier tipo: primitivas, arrays, objetos, booleanos, funciones, etc. Las propiedades están separadas por una coma.
+
+Las reglas para nombrar las llaves son sencillas:
+
+Si la clave va entre comillas, puede ser una cadena arbitraria.
+Si no hay comillas, se aplican las restricciones: el nombre no tiene espacios, empieza por una letra o los caracteres _ y $.
+*/
 const book = {
 	title: "The Last Kigndom",
 	author: "Bernard Cornwell",
@@ -10,6 +19,10 @@ console.log(book);
 console.log(`El precio del libro ${book.title} es ${book.price}`);
 book.genres.push("real life");
 console.log(book);
+
+/*Propiedades anidadas
+El valor de la propiedad puede ser otro objeto, para almacenar datos anidados y agrupados. Por ejemplo, las estadísticas de los usuarios de las redes sociales consisten en el número de seguidores, vistas y gustos, y lo más conveniente es almacenar estos datos como un objeto. Lo mismo con la ubicación, por separado el país y la ciudad.
+*/
 
 const user = {
 	name: "Jacques Gluke",
@@ -24,6 +37,13 @@ const user = {
 		likes: 1308,
 	},
 };
+/*Referencia a las propiedades a través de un punto
+
+La primera forma de acceder a una propiedad de un objeto es con la sintaxis objeto.nombre_propiedad. La sintaxis «a través del punto» se utiliza en la mayoría de los casos y es apropiada cuando conocemos de antemano el nombre (clave) de la propiedad a la que queremos acceder.
+
+El valor de la propiedad con ese nombre será devuelto en lugar de la referencia.
+Si no hay ninguna propiedad con ese nombre en el objeto undefined.
+*/
 
 console.log(user);
 console.log(user.location);
