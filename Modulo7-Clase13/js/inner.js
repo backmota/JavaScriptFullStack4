2316b17e28a540bc4200566788304ac65263744e
José Juan Mota <backmota@BackChai.local>
2022-12-12 20:21:14 -0600

    Clase 13

--- Modulo7-Clase13/js/inner.js
+++ Modulo7-Clase13/js/inner.js
@@ -1,1 +1,25 @@
-inner.js
+//Propiedad innerHTML
+
+//https://developer.mozilla.org/es/docs/Web/API/Element/innerHTML
+
+//Otra forma de crear elementos DOM y colocarlos en un árbol es utilizar las cadenas con tags y dejar que el navegador haga todo el trabajo pesado. Este enfoque tiene sus pros y sus contras.
+
+//Lectura
+//La propiedad innerHTML almacena el contenido de un elemento, incluyendo los tags, como una cadena. El valor de retorno es siempre un código HTML válido.
+
+const article = document.querySelector(".article");
+console.log(article.innerHTML);
+
+const title = document.querySelector(".article .title");
+console.log(title.innerHTML);
+
+const text = document.querySelector(".article .text");
+console.log(text.innerHTML);
+
+const link = document.querySelector(".article .link");
+console.log(link.innerHTML);
+
+//Cambio
+//La propiedad innerHTML se puede leer y escribir. Si escribe una cadena con tags HTML, el navegador analizará la cadena y las convertirá en elementos válidos y los añadirá al árbol DOM.
+
+title.innerHTML = 'Clase Número 13 de JavaScript En <span class="accent">Goit</span>';
