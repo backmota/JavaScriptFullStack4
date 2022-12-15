b13b22ac3d350fab58a3c5a9f932149386ca071f
José Juan Mota <backmota@BackChai.local>
2022-12-14 20:07:46 -0600

    Clase 14

---
+++ Modulo7-Clase14/js/event.js
@@ -0,0 +1,11 @@
+const form = document.querySelector(".register-form");
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const {
+		elements: { username, password}
+	} = event.currentTarget;
+	console.log("Event:",event);
+	console.log("currentTarget:",event.currentTarget);
+	console.log(username.value, password.value);
+} );
